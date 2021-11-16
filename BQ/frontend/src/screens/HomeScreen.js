c1e7c0d395c060a76f2f42fb60198e4a352e55df
VoThiNa2010 <na.vo2010@hcmut.edu.vn>
2021-11-17 00:43:00 +0700

    fix bug

--- BQ/frontend/src/screens/HomeScreen.js
+++ BQ/frontend/src/screens/HomeScreen.js
@@ -13,6 +13,7 @@ const HomeScreen = () => {
   const dispatch = useDispatch();
 
   const productList = useSelector(state => state.productList);
+  
   const { loading, error, products } = productList;
 
   useEffect(() => {
