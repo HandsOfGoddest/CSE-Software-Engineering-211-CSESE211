2fd79714162dc36e2b4291d5661bd50910121699
luanle912 <luan.le912@hcmut.edu.vn>
2021-11-17 10:13:25 +0700

    cart

--- BQ/frontend/src/store.js
+++ BQ/frontend/src/store.js
@@ -2,14 +2,19 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {productListReducer, productDetailsReducer} from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
-    
+    productDetails: productDetailsReducer,
+    cart: cartReducer
 })
 
-const initialState = {}
+const cartItemFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')):[]
+
+const initialState = {
+    cart: { cartItems: cartItemFromStorage },
+}
 
 const middleware = [thunk]
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
