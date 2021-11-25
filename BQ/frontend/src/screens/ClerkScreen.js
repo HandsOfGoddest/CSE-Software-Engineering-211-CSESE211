efa8efb71f9ba4053b82c733a8d6077c00f2bc43
luanle912 <luan.le912@hcmut.edu.vn>
2021-11-25 15:29:59 +0700

    25/11 3

---
+++ BQ/frontend/src/screens/ClerkScreen.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import { Form, Button, Row, Col, Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { LinkContainer } from 'react-router-bootstrap'
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { getUserDetails, updateUserProfile } from "../actions/userActions";
+import { listMyOrders } from "../actions/orderActions";
+
+const ProfileScreen = ({ history }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, error, user } = userDetails;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
+
+  const orderListMy = useSelector((state) => state.orderListMy);
+  const { loading: loadingOrders, error: errorOrders, orders } = orderListMy;
+
+  useEffect(() => {
+    if (!userInfo) {
+      history.push("/login");
+    } else {
+      if (!user?.name) {
+        dispatch(getUserDetails('profile'));
+        dispatch(listMyOrders())
+        
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+      }
+    }
+  }, [dispatch, history, userInfo, user]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage("MẬT KHẨU KHÔNG KHỚP");
+    } else {
+      dispatch(updateUserProfile({ id: user._id, name, email, password }));
+    }
+  };
+
+  return(
+    <Row style = {{marginTop:"100px"}}>
+      <Col md={12}>
+        <h2>Đơn hàng cần xác nhận</h2>
+        {loadingOrders ? <Loader/> : errorOrders ? <Message variant='danger'>{errorOrders}</Message> : (
+          <Table striped bordered hover responsive className='table-sm'>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map(order => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
+                  <td>{order.isPaid ? order.paidAt.substring(0,10): (
+                    <i className='fas fa-times' style={{color: 'red'}}></i>
+                  )}</td>
+                  <td>{order.isDelivered ? order.deliveredAt.substring(0,10): (
+                    <i className='fas fa-times' style={{color: 'red'}}></i>
+                  )}</td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <Button variant='light'>Details</Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </Col>
+    </Row>
+  )
+}
+
+export default ProfileScreen;
